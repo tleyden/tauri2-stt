6265465b942d529e57be21ec9625fa6d8c801f69
Traun Leyden <traun.leyden@gmail.com>
2026-07-06 13:22:37 +0200

    show the hardware used

--- argmax-oss-swift/swift-only-poc/Sources/WhisperCLIPocCore/TranscriptionRunner.swift
+++ argmax-oss-swift/swift-only-poc/Sources/WhisperCLIPocCore/TranscriptionRunner.swift
@@ -11,6 +11,10 @@ public enum TranscriptionRunner {
     )
     let whisperKit = try await WhisperKit(config)
 
+    if options.verbose {
+      logHardwareConfiguration(for: whisperKit)
+    }
+
     let decodingOptions = DecodingOptions(
       withoutTimestamps: false,
       wordTimestamps: options.wordTimestamps
@@ -52,5 +56,20 @@ public enum TranscriptionRunner {
     }
 
     return transcripts
+  }
+
+  private static func logHardwareConfiguration(for whisperKit: WhisperKit) {
+    let compute = whisperKit.modelCompute
+    fputs(
+      """
+      Hardware configuration:
+        - Mel spectrogram: \(compute.melCompute.displayName) (\(compute.melCompute.description))
+        - Audio encoder: \(compute.audioEncoderCompute.displayName) (\(compute.audioEncoderCompute.description))
+        - Text decoder: \(compute.textDecoderCompute.displayName) (\(compute.textDecoderCompute.description))
+        - Prefill: \(compute.prefillCompute.displayName) (\(compute.prefillCompute.description))
+
+      """,
+      stderr
+    )
   }
 }
